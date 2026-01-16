af06164e22e412167faaf9ca8fb5794e07711679
TD-WEBSITE-0832 <abdullayvsaidxon76@gmail.com>
2026-01-16 21:20:18 +0500

    heyhey

---
+++ script.js
@@ -0,0 +1,10 @@
+
+  const cards = document.querySelectorAll('.card3');
+
+  cards.forEach(card => {
+    card.addEventListener('click', () => {
+      cards.forEach(c => c.classList.remove('active'));
+      card.classList.add('active');
+    });
+  });
+
