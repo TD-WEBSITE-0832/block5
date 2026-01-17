690ac11eb853388cade12c6810e608aa5943e1a6
TD-WEBSITE-0832 <abdullayvsaidxon76@gmail.com>
2026-01-17 22:50:17 +0500

    no kill

--- script.js
+++ script.js
@@ -8,3 +8,32 @@
     });
   });
 
+
+
+  const toggle = document.getElementById("billingToggle");
+
+const prices = document.querySelectorAll(".price");
+
+toggle.addEventListener("change", () => {
+  if (toggle.checked) {
+    // YEARLY
+    prices[0].innerHTML = "Free";
+    prices[1].innerHTML = "$150<span>/year</span>";
+    prices[2].innerHTML = "$250<span>/year</span>";
+  } else {
+    // MONTHLY
+    prices[0].innerHTML = "Free";
+    prices[1].innerHTML = "$15<span>/month</span>";
+    prices[2].innerHTML = "$25<span>/month</span>";
+  }
+});
+
+
+  const burger = document.querySelector('.sec');
+  const menu = document.querySelector('.id');
+
+  burger.addEventListener('click', () => {
+    menu.classList.toggle('active');
+  });
+
+
